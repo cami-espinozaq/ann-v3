1a3717b18537d3ec84406e56b9e0c604d964cab1
Camila Espinoza <camila.espinoza@skymetrix.com>
2025-10-05 19:28:18 +0200

    fixed carousel issues

--- src/containers/Gallery/DialogCarousel.tsx
+++ src/containers/Gallery/DialogCarousel.tsx
@@ -13,14 +13,19 @@ const DialogCarousel = ({ isOpen, setIsOpen, images }: { isOpen: boolean | numbe
       }}
     >
       <DialogContent
-        className={cn("!max-w-[90vw] md:!max-w-[70vw]", "p-0 md:p-8 flex flex-col items-center border-0 shadow-none", "focus-visible:outline-none")}
+        className={cn(
+          "!max-w-[90vw] md:!max-w-[70vw]",
+          "p-0 md:p-8 flex flex-col items-center border-0 shadow-none",
+          "focus-visible:outline-none",
+          "bg-transparent"
+        )}
         closeButtonClassName="-translate-y-8"
       >
         <DialogTitle className="hidden">Gallery</DialogTitle>
         <Carousel className="w-[90%] max-h-full mt-4" opts={{ align: "start", duration: 10, startIndex: start }}>
           <CarouselContent className="h-full">
             {images.map((img, i) => (
-              <CarouselItem key={i} className="w-full flex items-center max-h-full">
+              <CarouselItem key={i} className="w-full flex items-center justify-center max-h-full">
                 <img src={img} alt={`carousel-${i}`} className="w-full h-auto object-contain" />
               </CarouselItem>
             ))}
